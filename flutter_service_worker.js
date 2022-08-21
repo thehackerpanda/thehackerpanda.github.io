6b9df6c0e354e8b581edc4e254c88b8096fbfe18
thehackerpanda <73018785+thehackerpanda@users.noreply.github.com>
2022-08-21 22:52:02 +0530

    Updated minor

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -21,9 +21,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a267f01d1babe4823661456009acc7e2",
-"/": "a267f01d1babe4823661456009acc7e2",
-"main.dart.js": "f10e76a759575333da7f1d80fb64dfb0",
+"index.html": "38cf741de1e6e1efd519c0c43c880545",
+"/": "38cf741de1e6e1efd519c0c43c880545",
+"main.dart.js": "86a99e247886e46621f779eada8a3682",
 "manifest.json": "f618167d9e9d3454f4cae60ea90f04c9",
 "version.json": "eb158575df97ce92294ff3624caba780"
 };
