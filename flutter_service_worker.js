0117389bf14c39afe973053e0274d7b54a056274
thehackerpanda <73018785+thehackerpanda@users.noreply.github.com>
2022-08-21 22:01:27 +0530

    Fixed minor bugs

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -21,9 +21,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "9e0be3c27b591cd963d024e5a7b30ce0",
-"/": "9e0be3c27b591cd963d024e5a7b30ce0",
-"main.dart.js": "5ad90b31853cf8d545098be2f39a0033",
+"index.html": "a267f01d1babe4823661456009acc7e2",
+"/": "a267f01d1babe4823661456009acc7e2",
+"main.dart.js": "f10e76a759575333da7f1d80fb64dfb0",
 "manifest.json": "f618167d9e9d3454f4cae60ea90f04c9",
 "version.json": "eb158575df97ce92294ff3624caba780"
 };
